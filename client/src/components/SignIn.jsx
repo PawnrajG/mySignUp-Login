34337924ab9e0eba022275648699529e9d519a64
webDev-Learner <pawnmugi@gmail.com>
2024-10-14 21:08:31 +0530

    Fix: added form validation

--- client/src/components/SignIn.jsx
+++ client/src/components/SignIn.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import axios from 'axios';
+import React from 'react';
+import SendMail from './SendMail';
 
 const SignIn = ()=>{
     const [email, setEmail] = useState("");
@@ -32,11 +34,11 @@ const SignIn = ()=>{
         <div className="flex items-center justify-center">
             <form onSubmit={handleSignin} method="POST" className="w-96 h-80 p-5 my-40 bg-black text-white flex flex-col rounded-md shadow-lg shadow-neutral-900">
                 <h2 className="text-gray-500 text-2xl mx-3 my-2">Sign In</h2>
-                <input onChange={(input) => setEmail(input.target.value)} className="m-3 px-4 py-2 rounded-md bg-slate-600 hover:bg-slate-700 transition delay-75 hover:outline-cyan-500 outline-none" type="email" name="Email" placeholder="Email"/>
-                <input onChange={(input) => setPassword(input.target.value)} className="m-3 px-4 py-2 rounded-md bg-slate-600 hover:bg-slate-700 transition delay-75 hover:outline-cyan-500 outline-none" type="password" name="Password" placeholder="Password"/>
+                <input onChange={(input) => setEmail(input.target.value)} className="m-3 px-4 py-2 rounded-md bg-slate-600 hover:bg-slate-700 transition delay-75 hover:outline-cyan-500 outline-none" type="email" name="Email" placeholder="Email" required/>
+                <input onChange={(input) => setPassword(input.target.value)} className="m-3 px-4 py-2 rounded-md bg-slate-600 hover:bg-slate-700 transition delay-75 hover:outline-cyan-500 outline-none" type="password" name="Password" placeholder="Password" required/>
                 <div className="flex flex-col items-center justify-center m-2">
                     <button type="submit" className="text-md text-white text-center bg-cyan-500 px-2 py-1 rounded-md w-24 my-4 cursor-pointer">Sign In</button>
-                    <a href="#" className="m-2 text-center text-cyan-500 w-auto ">Forget Password?</a>
+                    <a href="#" className="m-2 text-center text-cyan-500 w-auto">Forget Password?</a>
                 </div>
             </form>
         </div>
